a4e6c62f308afce8c0c8936eef0d443cda110523
Nusrat <nusrattazin9081@gmail.com>
2025-01-18 21:43:36 +0600

    Commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server listening on ${PORT}`))
